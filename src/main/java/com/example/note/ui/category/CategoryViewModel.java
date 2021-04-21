8f18164e16eda077bead2c7441caff07048b4fb3
AQ1011 <anhquocngo1011@gmail.com>
2021-04-21 13:51:49 +0700

    add menu

---
+++ src/main/java/com/example/note/ui/category/CategoryViewModel.java
@@ -0,0 +1,21 @@
+package com.example.note.ui.category;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.Locale;
+
+public class CategoryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public CategoryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
